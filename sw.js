5b17daee42db2be9af7b066fc956b0b504cce72d
tecnologicarca-cmd <tecnologic.arca@gmail.com>
2026-09-15 14:28:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
    arquivo antigo do cache.
    ===================================================================== */
 
-var VERSAO = 'carway-v13.1.1';
+var VERSAO = 'carway-v14.0.0';
 
 var CACHE_SHELL = VERSAO + '-shell';
 var CACHE_EXTERNO = VERSAO + '-externo';
