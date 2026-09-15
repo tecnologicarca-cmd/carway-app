f458320dccc3b826237384473829140ba97dd95d
tecnologicarca-cmd <tecnologic.arca@gmail.com>
2026-09-14 23:45:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
    FIPE, documentos com vencimento, alertas persistentes, depreciacao.
    ===================================================================== */
 
-var VERSAO_FRONT = 9;
+var VERSAO_FRONT = 11;
 
 function dbVazio() {
   return { veiculos:[], viagens:[], despesas:[], abastecimentos:[], manutencoes:[], planos:[],
